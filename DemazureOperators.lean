52fe0398393d79b1e5fa08484b5c2dd85c9d0531
bolito2 <bolito2hd@gmail.com>
2024-04-16 17:10:34 +0200

    deploy the whole package

--- DemazureOperators.lean
+++ DemazureOperators.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `DemazureOperators` library.
 -- Import modules here that should be built as part of the library.
-import DemazureOperators.Basic
+import DemazureOperators.Demazure
+import DemazureOperators.DemazureAux
+import DemazureOperators.DemazureRelations
+import DemazureOperators.DemazureAuxRelations
--- DemazureOperators/Basic.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib.Data.Set.Function
-section
-
-
-lemma wario : 1 + 1 = 2 := by simp
