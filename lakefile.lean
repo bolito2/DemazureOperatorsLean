225513c63908d286bcbe1f40f03ef81fd629430b
Óscar <bolito2hd@gmail.com>
2024-07-08 11:50:40 +0200

    made it work on mac without doc4-gen

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,12 @@
 import Lake
 open Lake DSL
 
-package «demazure_operators» where
-  -- add any package configuration options here
+package «DemazureOperators» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
@@ -10,6 +14,3 @@ require mathlib from git
 @[default_target]
 lean_lib «DemazureOperators» where
   -- add any library configuration options here
-
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
