46bab26f943c1905ed7296faa9c4fbe23cd7604a
Óscar <bolito2hd@gmail.com>
2024-10-19 17:02:17 +0200

    f is liftable skeleton

---
+++ DemazureOperators/SymmetricGroup.lean
@@ -0,0 +1,19 @@
+import DemazureOperators.Matsumoto
+
+variable (n : ℕ)
+def M := CoxeterMatrix.Aₙ n
+def cs := (M n).toCoxeterSystem
+def W := (M n).Group
+
+abbrev S (n : ℕ) := Equiv.Perm (Fin n)
+instance : Group (S (n + 1)) := Equiv.Perm.permGroup
+instance : Monoid (S (n + 1)) := sorry
+
+def f_simple : Fin n → S (n + 1) :=
+  fun i => Equiv.swap i (i + 1)
+
+
+theorem f_liftable : @CoxeterMatrix.IsLiftable (Fin n) (S (n+1)) infer_instance (M n) (f_simple n) := sorry
+
+def equiv : W n → S (n + 1) :=
+  lift
